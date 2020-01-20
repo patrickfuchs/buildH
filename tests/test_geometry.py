ac10e740ec6ac8d876af96bdc88b15fbcc78db0d
Hubert <hubert.santuz@gmail.com>
2020-01-20 16:35:26 +0100

    Add unit tests and CI

---
+++ tests/test_geometry.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Unit tests for buildH_calcOP
+
+Test functions from module geomtry
+"""
+
+import numpy as np
+from numpy.testing import (
+    assert_almost_equal,
+)
+import pytest
+
+import geometry
+
+
+@pytest.mark.parametrize('atom1, atom2, atom3, result', [
+    (np.array([21.13, 41.14, 31.36]), np.array([20.64, 39.8, 31.91]),
+     np.array([20.4, 39.52, 33.25]), 2.18789998),
+])
+def test_angle(atom1, atom2, atom3, result):
+    assert_almost_equal(geometry.calc_angle(atom1, atom2, atom3), result)
