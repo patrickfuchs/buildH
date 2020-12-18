bfaa0a304d268dd45717510ebdce39cb2d261441
Hubert <hubert.santuz@gmail.com>
2020-12-18 14:33:58 +0100

    Add tests for fast functions in the core module

--- tests/test_core.py
+++ tests/test_core.py
@@ -72,6 +72,42 @@ class TestPDBPOPC:
                                                                                   self.dic_lipid)
 
 
+    @pytest.mark.parametrize('atom_index, helpers_indexes', [
+         # atom C1
+         (0, (3,4)),
+         # atom C13
+         (12, (11, 31, 13)),
+         # atom C24
+         (23, (22, 24)),
+         # atom C50
+         (49, (48, 47)),
+    ])
+    def test_get_indexes(self, atom_index, helpers_indexes):
+        """
+        Test for get_indexes()
+        """
+        # Use the value in parametrize to get the correct atom to test.
+        atom = self.universe_woH.atoms[atom_index]
+
+        assert core.get_indexes(atom, self.dic_lipid) == helpers_indexes
+
+
+    def test_make_dic_lipids_with_indexes(self):
+        """
+        Test for make_dic_lipids_with_indexes
+        """
+
+        dic_lipids_with_indexes = core.make_dic_lipids_with_indexes(self.universe_woH,
+                                                                     self.dic_lipid,
+                                                                     self.dic_OP)
+
+        assert dic_lipids_with_indexes['C1']  == ('CH3', 'N4', 'C5', 0, 3, 4)
+        assert dic_lipids_with_indexes['C5']  == ('CH2', 'N4', 'C6', 4, 3, 5)
+        assert dic_lipids_with_indexes['C13'] == ('CH', 'C12', 'C32', 'O14', 12, 11, 31, 13)
+        assert dic_lipids_with_indexes['C24'] == ('CHdoublebond', 'C23', 'C25', 23, 22, 24)
+        assert dic_lipids_with_indexes['C50'] == ('CH3', 'C49', 'C48', 49, 48, 47)
+
+
     @pytest.mark.parametrize('Cname, index, Hs_coords', [
             # type CH3
             ("C1", 0, (np.array([35.06161421, 47.69320272, 26.76728762]),
